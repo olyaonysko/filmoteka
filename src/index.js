ab4ae7a2dd43a5ec568ce1e8cc57243eb5ecfedc
Mykola Osadchyi <osadchiy_n@ukr.net>
2020-11-04 14:30:02 +0200

    edited index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import './sass/styles.scss';
 import './js/1initialHomePage';
-import './js/2searchAndPlaginationHomePage';
 import './js/3navigation';
 import './js/4filmDetailsPage';
 import './js/5libraryPage';
